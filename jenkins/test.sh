2141943e2105dd62d6a558f80f5daab289050dbe
Vidya A Chhabria <chhab011@nyad.ece.umn.edu>
2019-12-13 22:29:38 -0600

    Adding OpeNPDN tests to Docker

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -2,6 +2,7 @@
 set -e
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/pdngen/test/regression fast"
+docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/OpeNPDN/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/resizer/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "/OpenROAD/src/opendp/test/regression fast"
 docker run -v $(pwd):/OpenROAD openroad/openroad bash -c "cd /OpenROAD/src/replace/test && python3 regression.py run openroad"
